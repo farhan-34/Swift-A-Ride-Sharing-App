fcd26e0856a64d2599050de25e5ea0d1a6a0b8ce
farhan-34 <m.farhan134@gmail.com>
2021-12-10 14:31:35 +0500

    Created files structure

--- app/src/main/java/com/example/swift/frontEnd/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/swift/frontEnd/activities/SplashScreenActivity.kt
@@ -1,11 +1,12 @@
-package com.example.swift
+package com.example.swift.frontEnd.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.swift.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
