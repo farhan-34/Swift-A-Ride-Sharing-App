9e568d55b684aff7f51596f32d14dbf2c047c182
Arham Dilshad <l181036@lhr.nu.edu.pk>
2021-12-11 17:14:11 +0500

    Forg0t Password FrontEND completed

--- app/src/main/java/com/example/swift/frontEnd/activities/SendOtpForPasswordResetActivity.kt
+++ app/src/main/java/com/example/swift/frontEnd/activities/SendOtpForPasswordResetActivity.kt
@@ -1,8 +1,10 @@
 package com.example.swift.frontEnd.activities
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.swift.R
 import kotlinx.android.synthetic.main.activity_send_otp_for_password_reset.*
@@ -14,7 +16,19 @@ class SendOtpForPasswordResetActivity : AppCompatActivity() {
         setContentView(R.layout.activity_send_otp_for_password_reset)
 
         btn_send_OTP_passReset.setOnClickListener {
-            startActivity(Intent(this, ReceiveOtpForPasswordResetActivity::class.java))
+            when {
+                Otp_phoneNumber_passReset.text == null -> {
+                    Toast.makeText(applicationContext, "Enter Mobile", Toast.LENGTH_SHORT).show()
+                }
+                Otp_phoneNumber_passReset.text.toString().trim().isEmpty() -> {
+                    Toast.makeText(applicationContext, "Enter Mobile", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    var intent = Intent(this, ReceiveOtpForPasswordResetActivity::class.java)
+                    intent.putExtra("mobile", Otp_phoneNumber_passReset.text.toString())
+                    startActivity(intent)
+                }
+            }
         }
     }
 
