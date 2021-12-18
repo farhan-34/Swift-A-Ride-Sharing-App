1aa94296f0093dab67f67a4f7ec5682f5355878c
Arham Dilshad <l181036@lhr.nu.edu.pk>
2021-12-18 15:46:54 +0500

    commit

--- app/src/main/java/com/example/swift/frontEnd/activities/DriverRegistrationActivity.kt
+++ app/src/main/java/com/example/swift/frontEnd/activities/DriverRegistrationActivity.kt
@@ -1,13 +1,25 @@
 package com.example.swift.frontEnd.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.swift.R
+import kotlinx.android.synthetic.main.activity_driver_registration.*
+import kotlinx.android.synthetic.main.activity_rider_register.*
 
 class DriverRegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_registration)
         supportActionBar?.hide()
+
+        val items = listOf("Bike", "Rickshaw", "Car")
+        val adapter = ArrayAdapter(this, R.layout.gender_dropdown_list_item, items)
+        registerDriver_VehicleTypeInput.setAdapter(adapter)
+
+        DriverRegister_button.setOnClickListener{
+            startActivity(Intent(this, DriverMainActivity::class.java))
+        }
     }
 }
