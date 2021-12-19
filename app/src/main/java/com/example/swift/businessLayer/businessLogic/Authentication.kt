9c6f6e5f89773874780affac61b66960a289848b
Lucifer <taimoorbajwa62@gmail.com>
2021-12-19 21:14:03 +0500

    firestore connected

--- app/src/main/java/com/example/swift/businessLayer/businessLogic/Authentication.kt
+++ app/src/main/java/com/example/swift/businessLayer/businessLogic/Authentication.kt
@@ -1,3 +1,18 @@
 package com.example.swift.businessLayer.businessLogic
 
-class Authentication 
+import com.example.swift.businessLayer.frontendInterfaces.AuthenticationInterface
+
+class Authentication :AuthenticationInterface{
+    override fun logIn(phoneNumber: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun otpVerification(phoneNumber: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setNewPassword(phoneNumber: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+}
