9587e0c5c0f7962bce7b72f3a6ea79174ceaa5f2
AbdullahAtGitHub <muhammedabdulla340@gmail.com>
2021-12-19 20:22:47 +0500

    added functions to ride manager (continued)

--- app/src/main/java/com/example/swift/businessLayer/businessLogic/RiderManager.kt
+++ app/src/main/java/com/example/swift/businessLayer/businessLogic/RiderManager.kt
@@ -1,10 +1,12 @@
 package com.example.swift.businessLayer.businessLogic
 
+import com.example.swift.backEnd.backEndClasses.RegistrationImp
+
 class RiderManager (){
     fun createRider(name:String, gender:String, age:Int, email:String, phoneNumber:String, password:String){
 
-
-
+        val obj1 = RegistrationImp()
+        obj1.createRider(name, gender, age, email, phoneNumber, password)
 
 
 
