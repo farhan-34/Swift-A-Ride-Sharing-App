9add05429a9a1134a0053eb0493d1eb4e21a9568
Lucifer <taimoorbajwa62@gmail.com>
2021-12-19 23:07:50 +0500

    firestore connected (Continued)

--- app/src/main/java/com/example/swift/backEnd/backEndClasses/AuthenticationImp.kt
+++ app/src/main/java/com/example/swift/backEnd/backEndClasses/AuthenticationImp.kt
@@ -1,10 +1,12 @@
 package com.example.swift.backEnd.backEndClasses
 
 import com.example.swift.backEnd.backEndInterfaces.Authentication
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AuthenticationImp :Authentication{
     override fun logIn(phoneNumber: String, password: String) {
         TODO("Not yet implemented")
+        var db = FirebaseFirestore.getInstance()
     }
 
     override fun otpVerification(phoneNumber: String) {
