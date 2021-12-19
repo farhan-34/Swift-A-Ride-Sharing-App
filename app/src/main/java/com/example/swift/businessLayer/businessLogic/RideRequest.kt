71ccd78b73bef4c8fb47da9ebc5be8996e6c9143
AbdullahAtGitHub <muhammedabdulla340@gmail.com>
2021-12-19 19:07:40 +0500

    added ride request class

--- app/src/main/java/com/example/swift/businessLayer/businessLogic/RideRequest.kt
+++ app/src/main/java/com/example/swift/businessLayer/businessLogic/RideRequest.kt
@@ -1,3 +1,13 @@
 package com.example.swift.businessLayer.businessLogic
 
-class RideRequest 
+class RideRequest(
+    var requestId:String,
+    var riderId:String,
+    var riderName:String,
+    var riderRating: String,
+    var sourceLocation:String,
+    var destinationLocation:String,
+    var vehicleType:String
+){
+    fun createRequest() {}
+}
