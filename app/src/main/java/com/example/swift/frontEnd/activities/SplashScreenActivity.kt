05684dbf69d316e954196ea4aec6d637a6f69af6
Arham Dilshad <l181036@lhr.nu.edu.pk>
2022-01-02 01:16:14 +0500

    location permission added

--- app/src/main/java/com/example/swift/frontEnd/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/swift/frontEnd/activities/SplashScreenActivity.kt
@@ -1,14 +1,23 @@
 package com.example.swift.frontEnd.activities
 
+import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.swift.R
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
@@ -23,86 +32,86 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private var mInterstitialAd: InterstitialAd? = null
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestedOrientation =  (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         setContentView(R.layout.activity_splash_screen)
         supportActionBar?.hide()
 
 
-
-        // TODO: uncomment and Set correct value later
-        val topAnim = AnimationUtils.loadAnimation(this,R.anim.car_logo_animation)
-        val bottomAnim = AnimationUtils.loadAnimation(this,R.anim.swift_logo_animation)
+        val topAnim = AnimationUtils.loadAnimation(this, R.anim.car_logo_animation)
+        val bottomAnim = AnimationUtils.loadAnimation(this, R.anim.swift_logo_animation)
 
         splash_car.startAnimation(topAnim)
         splash_swift.startAnimation(bottomAnim)
 
 
+        val splashScreenTimeOut = 2000
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+
+                gotoSignin()
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        }
 
 
 
-        var adRequest = AdRequest.Builder().build()
-        InterstitialAd.load(this,getString(R.string.Swift_Frontpage_ad_Testid), adRequest, object : InterstitialAdLoadCallback() {
-            override fun onAdFailedToLoad(adError: LoadAdError) {
-                Log.d(ContentValues.TAG, adError?.message)
-                mInterstitialAd = null
-                var homeIntent = Intent(this@SplashScreenActivity,SignInActivity::class.java)
-                val splashScreenTimeOut = 1000
-
-                Handler(Looper.getMainLooper()).postDelayed({
-
-                    startActivity(homeIntent)
-                    finish()
-                }, splashScreenTimeOut.toLong())
-
-            }
-
-            override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                Log.d(ContentValues.TAG, "Ad was loaded.")
-                mInterstitialAd = interstitialAd
-                mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
-                    override fun onAdFailedToShowFullScreenContent(p0: AdError) {
-                        super.onAdFailedToShowFullScreenContent(p0)
-                        var homeIntent = Intent(this@SplashScreenActivity,SignInActivity::class.java)
-                        val splashScreenTimeOut = 1000
-
-                        Handler(Looper.getMainLooper()).postDelayed({
-
-                            startActivity(homeIntent)
-                            finish()
-                        }, splashScreenTimeOut.toLong())
-                    }
-
-                    override fun onAdDismissedFullScreenContent() {
-
-                        super.onAdDismissedFullScreenContent()
-                        Log.d(ContentValues.TAG, "Ad was Started.")
-                        var homeIntent = Intent(this@SplashScreenActivity,SignInActivity::class.java)
-                        val splashScreenTimeOut = 10
-
-                        Handler(Looper.getMainLooper()).postDelayed({
-
-                            startActivity(homeIntent)
-                            finish()
-                        }, splashScreenTimeOut.toLong())
-
-
-                    }
-
-
-                }
-                mInterstitialAd?.show(this@SplashScreenActivity)
-
-
-            }
-
-
-        })
-
+        }, splashScreenTimeOut.toLong())
 
 
     }
+
+    fun gotoSignin(){
+        var homeIntent = Intent(this@SplashScreenActivity, SignInActivity::class.java)
+        startActivity(homeIntent)
+        finish()
+    }
+
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                gotoSignin()
+                // app.
+            } else {
+               finish()
+            }
+        }
+
+
+    //permission
+//    private val PERMISSIONS_FINE_LOCATION = 50
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        @NonNull permissions: Array<String>,
+//        @NonNull grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        when ((requestCode)) {
+//            PERMISSIONS_FINE_LOCATION -> {
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//
+//                } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+//                    shouldShowRequestPermissionRationale()
+//                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        }
+//    }
 
 
 }
