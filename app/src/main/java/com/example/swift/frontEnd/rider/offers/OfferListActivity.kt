58c662a694fb9878f2390718a2f3aeef703649dc
farhan-34 <m.farhan134@gmail.com>
2022-02-25 15:20:25 +0500

    flow changed and drive online service added

--- app/src/main/java/com/example/swift/frontEnd/rider/offers/OfferListActivity.kt
+++ app/src/main/java/com/example/swift/frontEnd/rider/offers/OfferListActivity.kt
@@ -1,17 +1,11 @@
 package com.example.swift.frontEnd.rider.offers
 
-
-// romove this fragment later
-
-
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.swift.R
@@ -20,32 +14,26 @@ import com.example.swift.businessLayer.dataClasses.DriverOffer
 import com.example.swift.businessLayer.session.RiderSession
 import com.example.swift.frontEnd.Services.NotifyOnDriverOffer
 import com.example.swift.frontEnd.rider.homePage.RiderHomePageFragment
+import com.example.swift.frontEnd.rider.riderMain.RiderMainActivity
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_offer_list.*
 import kotlinx.android.synthetic.main.fragment_rider_offer_list.*
 
-private lateinit var driversOffersList : ArrayList<DriverOffer>
-private  lateinit var driversOffersRecyclerView: RecyclerView
-private lateinit var adapter: OfferListAdapter
-class RiderOfferListFragment : Fragment() {
+class OfferListActivity : AppCompatActivity() {
+
+    private lateinit var driversOffersList : ArrayList<DriverOffer>
+    private  lateinit var driversOffersRecyclerView: RecyclerView
+    private lateinit var adapter: OfferListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offer_list)
+        requestedOrientation =  (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        supportActionBar?.hide()
+
         driversOffersList = ArrayList<DriverOffer>()
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rider_offer_list, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        driversOffersRecyclerView =  view.findViewById(R.id.driversOffers_recycler)
+        driversOffersRecyclerView = findViewById(R.id.driversOffers_recycler)
         load_data()
         init_recycler_view()
 
@@ -67,7 +55,7 @@ class RiderOfferListFragment : Fragment() {
                     }
 
                     override fun onCancelled(databaseError: DatabaseError) {
-                        Log.e(TAG, "onCancelled", databaseError.toException())
+                        Log.e(ContentValues.TAG, "onCancelled", databaseError.toException())
                     }
                 })
 
@@ -85,35 +73,35 @@ class RiderOfferListFragment : Fragment() {
                     }
 
                     override fun onCancelled(databaseError: DatabaseError) {
-                        Log.e(TAG, "onCancelled", databaseError.toException())
+                        Log.e(ContentValues.TAG, "onCancelled", databaseError.toException())
                     }
                 })
             }
             //closing service
             Common.endThread = true
-            val intent = Intent(requireContext(), NotifyOnDriverOffer::class.java)
-            requireContext().stopService(intent)
+            val intent = Intent(this, NotifyOnDriverOffer::class.java)
+            this.stopService(intent)
 
             //switching to home fragment
-            parentFragmentManager.beginTransaction().replace(R.id.rider_main_fragment_container, RiderHomePageFragment()).addToBackStack(null).commit()
+            startActivity(Intent(this, RiderMainActivity::class.java))
+            this.finish()
         }
     }
-
 
     private fun load_data(){
         var db = FirebaseDatabase.getInstance().getReference().child("RiderOffers")
 
         RiderSession.getCurrentUser { rider ->
-            db.addChildEventListener(object:ChildEventListener{
+            db.addChildEventListener(object: ChildEventListener {
                 override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                        val i = snapshot.value.toString()
-                        val temp: DriverOffer? = snapshot.getValue(DriverOffer::class.java)
+                    val i = snapshot.value.toString()
+                    val temp: DriverOffer? = snapshot.getValue(DriverOffer::class.java)
 
-                        if (temp != null) {
-                            if (temp.riderId == rider.riderId) {
-                                driversOffersList.add(temp!!)
-                            }
+                    if (temp != null) {
+                        if (temp.riderId == rider.riderId) {
+                            driversOffersList.add(temp!!)
                         }
+                    }
 
                     adapter.notifyDataSetChanged()
                 }
@@ -148,10 +136,8 @@ class RiderOfferListFragment : Fragment() {
     }
 
     private fun init_recycler_view(){
-        adapter = OfferListAdapter(requireContext(), driversOffersList)
+        adapter = OfferListAdapter(this, driversOffersList)
         driversOffersRecyclerView.adapter = adapter
-        driversOffersRecyclerView.layoutManager = LinearLayoutManager(view?.context)
+        driversOffersRecyclerView.layoutManager = LinearLayoutManager(this)
     }
-
-
 }
