91e34175ee9a0054e7a0f72c7d1d62e84db35261
Lucifer <taimoorbajwa62@gmail.com>
2022-04-12 22:12:58 +0500

    panic button fragment minor updates update button added

--- app/src/main/java/com/example/swift/frontEnd/rider/panicButton/PanicButtonFragment.kt
+++ app/src/main/java/com/example/swift/frontEnd/rider/panicButton/PanicButtonFragment.kt
@@ -2,17 +2,22 @@ package com.example.swift.frontEnd.rider.panicButton
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.telephony.SmsManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.get
 import com.example.swift.R
 import com.example.swift.businessLayer.session.RiderSession
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_panic_button.*
+import kotlinx.android.synthetic.main.activity_rider_register.*
+import net.rimoto.intlphoneinput.IntlPhoneInput
 
 
 class PanicButtonFragment : Fragment() {
@@ -33,6 +38,7 @@ class PanicButtonFragment : Fragment() {
         val db = Firebase.firestore
 
         btn_add_panic_contact.setOnClickListener {
+            if(isInputsValid()) {
             val phoneNumber = emergency_contact_input.text.toString()
             RiderSession.getCurrentUser { rider ->
                 db.collection("EmergencyContact").document(rider.phoneNumber).get()
@@ -40,13 +46,38 @@ class PanicButtonFragment : Fragment() {
                         // getting current phone numbers list
                         var phoneNumberList:String = document.get("contacts").toString()
                         // adding the new phone number to the list
-                        phoneNumberList += ",$phoneNumber"
+                        if(phoneNumber !in phoneNumberList) {
+                            phoneNumberList += ",$phoneNumber"
+
                         val contacts = mapOf(
                             "contacts" to phoneNumberList
                         )
                         // pushing the list to the database
                         db.collection("EmergencyContact").document(rider.phoneNumber).set(contacts)
+                            .addOnSuccessListener {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Contact Added!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            .addOnFailureListener{
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Contact not Added!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                        else{
+                            Toast.makeText(
+                                requireContext(),
+                                "Contact already exist!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
+            }
             }
         }
         panic_button.setOnClickListener {
@@ -67,5 +98,36 @@ class PanicButtonFragment : Fragment() {
                     }
             }
         }
+    }
+    private fun isInputsValid(): Boolean {
+        var isValid = true
+        var flag = 1
+        if(emergency_contact_input.text == null) {
+            showPhoneError()
+            Toast.makeText(requireContext(), "Enter Mobile", Toast.LENGTH_SHORT).show()
+            isValid = false
+        }
+        else if(emergency_contact_input.text.toString().trim().isEmpty()) {
+            showPhoneError()
+            Toast.makeText(requireContext(), "Enter Mobile", Toast.LENGTH_SHORT).show()
+            isValid = false
+        }
+        else {
+            removePhoneError()
+        }
+        return isValid
+    }
+
+    private fun showPhoneError(){
+        val border = GradientDrawable()
+        border.setStroke(3, -0x4f0000) //red border with full opacity
+        border.cornerRadius = 50F
+        emergency_contact_input.setBackgroundDrawable(border)
+    }
+    private fun removePhoneError(){
+        val border = GradientDrawable()
+        border.setStroke(3, -0xFFA500) //reset color
+        border.cornerRadius = 50F
+        emergency_contact_input.setBackgroundDrawable(border)
     }
 }
